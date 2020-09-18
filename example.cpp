0b9aebce9996aa6c49c1792ebc49927f3e151de9
kimaya <kimaya@localhost.localdomain>
2020-09-18 12:26:42 +0530

    getting started with boost library to study design pattern

---
+++ example.cpp
@@ -0,0 +1,27 @@
+#include <boost/lambda/lambda.hpp>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+int main()
+{
+    using namespace boost::lambda;
+    typedef std::istream_iterator<int> in;
+
+    std::for_each(
+        in(std::cin), in(), std::cout << (_1 * 3) << " " );
+}
+
+/*
+
+downloaded the boost library boost_1_61_0.tar.bz2
+extracted that file in the directory below
+
+installed boost library in the directory
+/usr/local
+
+used following command to compile this file
+
+g++ -I /usr/local/boost_1_61_0/ example.cpp -o example
+
+*/
